d1961149e5a7f6e860a1d5fd4251b062b53823e9
bear <1065751395@qq.com>
2026-03-31 12:36:43 +0000

    2026年3月31日 20:36

--- src/views/games/game2/game2.vue
+++ src/views/games/game2/game2.vue
@@ -26,7 +26,11 @@
         <div v-if="store.showPhoneSystem" class="wechat-system-wrapper">
           <WechatChat v-show="store.activePhonePage === 'chat'" />
           <OfficialAccount v-show="store.activePhonePage.startsWith('oa-')" />
-          <PhishingForm v-show="store.activePhonePage === 'welfare'" />
+          <!-- 将原来的 v-show 改为 v-if，并添加 key 确保重新创建 -->
+          <PhishingForm
+            v-if="store.activePhonePage === 'welfare'"
+            :key="store.activePhonePage"
+          />
           <ProfileFake v-show="store.activePhonePage === 'fake-profile'" />
           <ProfileReal v-show="store.activePhonePage === 'real-profile'" />
           <FakeMoments v-show="store.activePhonePage === 'fake-moments'" />
